83cbfc5d7b9664e31f64f9460247a1b766d282a9
Ryan Chen <y.f.chenroderick@gmail.com>
2023-07-31 13:57:05 +0800

    init: project init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vuepress@next 試玩
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress';
+
+export default defineUserConfig({
+  base: 'vuepress-test-1',
+  lang: 'en-US',
+  title: '你好， VuePress ！',
+  description: '這是我的VuePress2'
+});
